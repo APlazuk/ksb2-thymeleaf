f4697e06e3b2d57a30237633f4b69287d371f840
APlazuk <aleksandraplazuk@o2.pl>
2023-11-18 18:57:12 +0100

    add car adding form

--- src/main/java/pl/aplazuk/ksb2thymeleaf/CarController.java
+++ src/main/java/pl/aplazuk/ksb2thymeleaf/CarController.java
@@ -3,16 +3,49 @@ package pl.aplazuk.ksb2thymeleaf;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class CarController {
 
+    private  List<Car> cars;
+
+    public CarController() {
+        Car car1 = new Car("BMW", "X1");
+        Car car2 = new Car("Fiat", "126p");
+        Car car3 = new Car("Polonez", "Caro");
+        Car car4 = new Car("Mazda", "CX1");
+        cars = new ArrayList<>();
+        cars.add(car1);
+        cars.add(car2);
+        cars.add(car3);
+        cars.add(car4);
+    }
+
     @GetMapping("/car")
     public String getCar(Model model){
-        Car car = new Car("BMW", "X1");
-
-        model.addAttribute("name", "Bolka");
-        model.addAttribute("car", car);
+        model.addAttribute("cars", cars);
+        model.addAttribute("newCar", new Car());
         return "car";
     }
+
+    @PostMapping("/add-car")
+    public String addCar(@ModelAttribute Car car){
+        cars.add(car);
+        return "redirect:/car";
+    }
+
+    //przekazujemy wartosci jawnie
+    @GetMapping("/add-car")
+    public String addCar(@RequestParam String mark, @RequestParam String model){
+        cars.add(new Car(mark, model));
+//        return "redirect:/car";
+        return "car";
+    }
+
 }
